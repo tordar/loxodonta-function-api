50a36116efbb255a0ef876b67b63043f4e35ee04
Tordar Tømmervik <tordar.tommervik@redcross.no>
2024-10-25 15:39:01 +0200

    fixed unused request

--- src/app/api/hello/route.ts
+++ src/app/api/hello/route.ts
@@ -2,7 +2,7 @@ import { NextResponse } from 'next/server';
 
 export const runtime = 'edge'; 
 
-export async function GET(request: Request) {
+export async function GET() {
     const currentTime = new Date().toLocaleTimeString();
     const greeting = `Hello! The current time is ${currentTime}`;
 
